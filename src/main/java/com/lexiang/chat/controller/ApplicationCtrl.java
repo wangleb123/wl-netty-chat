a05bbd5179e3ff023318340a8b7e32fe38cfa080
王乐 <1660104869@qq.com>
2020-08-13 19:37:54 +0800

    1.整合wl-oauth-starter 2.profile多环境配置 3.webSocket代码编写

--- src/main/java/com/lexiang/chat/controller/ApplicationCtrl.java
+++ src/main/java/com/lexiang/chat/controller/ApplicationCtrl.java
@@ -3,15 +3,26 @@ package com.lexiang.chat.controller;
 import com.google.common.collect.Maps;
 import com.lexiang.chat.pojo.User;
 import com.lexiang.oauth.service.LoginService;
+import com.lexiang.oauth.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("user")
 public class ApplicationCtrl{
 
     @Autowired
     private LoginService loginService;
 
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping("/login")
     public void login(){
 
+        int a = 1;
+        redisService.set("dasd","da");
         loginService.login(Maps.newHashMap(),"chat",new User());
 
     }
